2d59874ae8e746ea80b4e11be61e4e9d0e990127
mahmudh <mahmudhssn246@gmail.com>
2016-03-23 18:13:32 +0000

    arrow function in src/index.js

--- src/index.js
+++ src/index.js
@@ -3,15 +3,21 @@
 
 import common from './modules/common/index'
 import book from './modules/book/index'
-import NavController from './modules/common/nav/navcontroller'
+import NavCtrl from './modules/common/nav/navcontroller'
 import bookstoreDirective from './modules/common/bookstore/bookstoreDirective'
 import bookservice from './modules/book/services/bookservice'
+import bookdetails from './modules/book/details/bookdetailscontroller'
+import bookAddCtrl from './modules//book/add/bookaddcontroller'
+import bookListCtrl from './modules//book/list/booklistcontroller'
 
   angular.module('bookstore', ['common','ngRoute','book', 'ngMockE2E']);
 
-  document.addEventListener('DOMContentLoaded',function(){
-    angular.bootstrap(document.body, ['bookstore']);
-  });
+  var mainBody = () => { angular.bootstrap(document.body, ['bookstore']); };
+
+  // document.addEventListener('DOMContentLoaded',() => {
+  //   angular.bootstrap(document.body, ['bookstore']);
+  // });
+
+  document.addEventListener('DOMContentLoaded', mainBody);
 
 // })();
-
